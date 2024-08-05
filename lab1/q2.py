5451d2880616822038bb387f554c65963e907a18
Satyam Bhattacharjee <115366950+satyam23b4@users.noreply.github.com>
2024-08-05 11:25:23 +0530

    q2_1.py

--- lab1/q2.py
+++ lab1/q2.py
@@ -1,22 +1,45 @@
-def generate_key(text, keyword):
-    key = list(keyword)
-    i = 0
-    for i in range(len(keyword)):
-        if i== len(keyword):
-            i=0
-        elif len(keyword)==len(text):
-            key.join(key[i])
-    return key
-        
-        
-def encrypt_vig(text, key):
-
-    result=""
-    for char in text:
+def generate_key(msg, key):
+    key = list(key)
+    if len(key)==len(msg):
+        return key
+    else:
+        for i in range(len(msg)-len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+def encrypt_vigenere(msg, key):
+    key = generate_key(msg, key)
+    encrypted_text = []
+    for i in range(len(msg)):
+        char = msg[i]
         if char.isupper():
-            start = ord('A')
+            encrypted_char = chr((ord(char)+ord(key[i])-2*ord('A'))%26+ord('A'))
+        elif char.islower():
+            encrypted_char = chr((ord(char)+ord(key[i])-2*ord('a'))%26+ord('a'))
         else:
-            start = ord('a')
-        encrypted_char = chr(((ord(char)-start) * key)%26 + start)
-        result += encrypted_char
-    return result
+            encrypted_char = char
+        encrypted_text.append(encrypted_char)
+    return "".join(encrypted_text)
+def decrypt_vigenere(msg, key):
+    key = generate_key(msg, key)
+    decrypted_text = []
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            decrypted_char = chr((ord(char)-ord(key) - ord('A'))%26 +ord('A'))
+        if char.islower():
+            decrypted_char = chr((ord(char)-ord(key) - ord('a'))%26 +ord('a'))
+        else:
+            encrypted_char = char
+        decrypted_text.append(decrypted_char)
+    return "".join(decrypted_text)
+msg = "the house is being sold tonight"
+key = "dollars"
+choice  = input("for encryption press E and for decryption press D\n")
+if choice == 'E':
+    encrypted_text = encrypt_vigenere(msg, key)
+    print(f"The encrypted message is: {encrypted_text}")
+elif choice == 'D':
+    decrypted_text = decrypt_vigenere(msg, key)
+    print(f"The decrypted message is: {decrypted_text}")
+else:
+    print("invalid choice")
